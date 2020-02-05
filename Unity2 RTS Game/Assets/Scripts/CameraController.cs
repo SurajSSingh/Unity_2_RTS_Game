ac585421b9c3742c96f60204889f7496a7658ef9
Suraj Singh <surajss@uci.edu>
2020-02-05 12:03:57 -0800

    Added Sprites and Testing Make
    Current Issue with Visual Studio Code and beta version, switching to
    Visual Studio

---
+++ Unity2 RTS Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float speed = 10.0f;
+    
+    void Start () 
+    {
+    }
+        
+    void Update () 
+    {
+        Vector3 ; 
+    }
+}
