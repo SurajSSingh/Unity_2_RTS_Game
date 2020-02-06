174adff40b120226da29e76e5ac3bf37b9f705fa
Suraj Singh <surajss@uci.edu>
2020-02-06 15:13:55 -0800

    Created Basic SO and FSM
    Will need to flesh out

--- Unity2 RTS Game/Assets/Scripts/CameraController.cs
+++ Unity2 RTS Game/Assets/Scripts/CameraController.cs
@@ -4,14 +4,54 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    float speed = 10.0f;
+    float speed = 0.25f;
+    float zSpace = -10.0f;
+    float maxHeight = 85f;
+    float maxWidth = 90;
+    float fullZoom = 25.0f;
+    bool zoomedOut = false;
+    Camera cam;
     
     void Start () 
     {
+        cam = this.GetComponent<Camera>();
     }
         
     void Update () 
     {
-        Vector3 ; 
+        this.transform.position = NewPostion();
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            this.transform.position = new Vector3(0.0f,0.0f,zSpace);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (zoomedOut)
+            {
+                zoomedOut = false;
+                cam.orthographicSize -= fullZoom;
+            }
+            else
+            {
+                zoomedOut = true;
+                cam.orthographicSize += fullZoom;
+            }
+        }
+
+    }
+
+    private Vector3 NewPostion()
+    {
+        Vector3 postion = this.transform.position;
+        // Clamp x and y postions with in arena size
+        float xPosChange = Mathf.Clamp(
+            postion.x + Input.GetAxis("Horizontal") * speed,
+            -maxHeight,
+            maxHeight);
+        float yPosChange = Mathf.Clamp(
+            postion.y + Input.GetAxis("Vertical") * speed,
+            -maxWidth,
+            maxWidth);
+        return new Vector3(xPosChange,yPosChange, zSpace);
     }
 }
---
+++ Unity2 RTS Game/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "CustomObjects/Enemy")]
+public class EnemyScript : ScriptableObject
+{
+    // Overrides the name attribute from ScriptableObject class
+    public new string name;
+
+    public int health;
+    public int speed;
+    public int damage;
+    public Sprite healthySprite;
+    public Sprite damagedSprite;
+    public Sprite nearDeathSprite;
+}
